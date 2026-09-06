703f2e280d1786852ae136f8b323a31530e4ce0a
Abomjneid <153464909+Abomjneid@users.noreply.github.com>
2026-09-07 00:11:44 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
 export default defineConfig({
+  base: '/nawwar-arabic-learning-app/', // صحيح لاستضافة المشروع على https://<user>.github.io/nawwar-arabic-learning-app
   plugins: [react()],
-  base: '/nawwar-arabic-learning-app/', // تأكد من وجود هذا السطر تماماً لإصلاح المسارات
 })
