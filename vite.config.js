032b9c421451a6b113ee4426764d77cd958626ae
Abomjneid <153464909+Abomjneid@users.noreply.github.com>
2026-09-07 01:02:02 +0300

    Clarify base URL in vite.config.js
    Updated base URL comment for clarity.

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/nawwar-arabic-learning-app/',
+  base: '/nawwar-arabic-learning-app/', // أو '/' إذا ستستضيف على root domain
   plugins: [react()],
 })
