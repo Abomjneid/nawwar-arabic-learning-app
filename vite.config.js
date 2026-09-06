e7281b85652d753eae65516bb49ab9778f74188e
Abomjneid <153464909+Abomjneid@users.noreply.github.com>
2026-09-07 00:20:31 +0300

    Fix comment for project hosting in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/nawwar-arabic-learning-app/', // صحيح لاستضافة المشروع على https://<user>.github.io/nawwar-arabic-learning-app
+  base: '/nawwar-arabic-learning-app/', // صحيح لصفحة المشروع
   plugins: [react()],
 })
